b2cb0b78bfa1fc09c083dd97de82fc62ac1dd554
Visnu <visnuram2004@gmail.com>
2021-04-29 10:57:00 +0530

    initial commit

---
+++ youtube_download.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from pytube import YouTube
+
+root = tk.Tk()
+root.geometry('500x500')
+root.resizable(0,0)
+root.title("POGCHAMP YOUTUBE DOWNLOADER")
+
+
+title_label=tk.Label(root,text = 'Youtube Video Downloader', font ='arial 20 bold').pack()
+
+
+yt_link = tk.StringVar()
+
+link_label=tk.Label(root, text = 'DROP THE LINK HERE: ', font = 'arial 15 bold').place(x= 32 , y = 60)
+yt_link_enter = tk.Entry(root, width = 70,textvariable = yt_link).pack(fill=tk.X,pady=60)
+
+
+def Yt_Downloader():
+
+    yt_url =YouTube(str(yt_link.get()))
+    yt_video = url.streams.first()
+    yt_video.download()
+    finished=tk.Label(root, text = 'DOWNLOADED COMPLETE  POGGERS!', font = 'arial 15').place(x= 50 , y = 210)
+
+
+
+button=tk.Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' ,bg = 'white', padx = 5,pady=5, command = Yt_Downloader).pack(fill=tk.X,pady=20)
+
+
+
+root.mainloop()
